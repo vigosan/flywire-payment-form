ef5b2c8f00a34a9659ca2e9f43a71628be1d2da3
Vicent Gozalbes <vigosan@gmail.com>
2020-09-19 13:52:04 +0200

    Fix className

--- src/components/Ribbon/Ribbon.js
+++ src/components/Ribbon/Ribbon.js
@@ -11,7 +11,7 @@ function Ribbon() {
         width="149"
         height="149"
         src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149"
-        class="attachment-full size-full"
+        className="attachment-full size-full"
         alt="Fork me on GitHub"
         data-recalc-dims="1"
         loading="lazy"
